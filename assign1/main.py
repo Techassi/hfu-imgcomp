340bc9d46e6811ef1885c21b0ee4a50b87827e82
Techassi <git@techassi.dev>
2022-04-03 13:16:10 +0200

    Adjust command options
    This changes the -p/--path option to -s/--source.
    
    Also the prettify command has an addtional flag -p/--preview to indicate if we want to display a preview window.

--- assign1/main.py
+++ assign1/main.py
@@ -15,7 +15,7 @@ def cli():
 
 @click.command('capture')
 @click.option('-n', '--count', default=3, help='Number of pictures')
-@click.option('-p', '--path', default='.data', help='Path to the data directory')
+@click.option('-s', '--source', default='.data', help='Path to the source directory')
 def capture_cmd(count: int, path: str):
     '''Capture a set of images for the calibration process'''
     capture.do(count, path)
@@ -28,11 +28,12 @@ def params_cmd():
 
 
 @click.command('prettify')
-@click.option('-p', '--path', default=".data", help='Path to the data directory')
+@click.option('-s', '--source', default=".data", help='Path to the source directory')
 @click.option('-r', '--results', default='.results', help='Path to the result directory')
-def prettify_cmd(path: str, results: str):
+@click.option('-p', '--preview', default=False, help='Display preview windows for the calibration and result images', is_flag=True)
+def prettify_cmd(source: str, results: str, preview: bool):
     '''Undistort live image from a camera'''
-    prettify.do(path, results)
+    prettify.do(source, results, preview)
 
 
 cli.add_command(prettify_cmd)
