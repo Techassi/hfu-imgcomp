818d8c8e4168c0a9f525fba6d2f59ffe123068ee
Techassi <git@techassi.dev>
2022-04-23 12:33:01 +0200

    Adjust wait utility function

--- assign2/utils/wait.py
+++ assign2/utils/wait.py
@@ -1,19 +1,16 @@
 import cv2 as cv
 
 
-def wait(d: int) -> bool:
-    '''Wait for <d> milliseconds and return if we want to exit
+def wait(d: int):
+    '''
+    Wait for 'd' milliseconds until we press <q>.
 
-    Args:
-        d (int): Delay in milliseconds
-
-    Returns:
-        exit (bool): True if the user wants to exit, otherwise False
+    Parameters
+    ----------
+    d : int
+        Delay in milliseconds
     '''
     while True:
         key = cv.waitKey(d)
-        if key == ord('n'):
-            return False
-
         if key == ord('q'):
-            return True
+            return
