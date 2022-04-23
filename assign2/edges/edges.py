061ce3d09ebfce5e270763ccebdeaf9a7ccbbe6c
Techassi <git@techassi.dev>
2022-04-23 12:33:18 +0200

    Start edge detection command

--- assign2/edges/edges.py
+++ assign2/edges/edges.py
@@ -1,2 +1,39 @@
-def do():
-    pass
+import cv2 as cv
+import click
+
+import utils.images as images
+import utils.input as inp
+import utils.wait as wait
+
+
+def do(base_path: str, max: int, min: int):
+    '''
+    Automatically detect edges in the source image to calculate vanishing line.
+
+    Parameters
+    ----------
+    base_path : str
+        Path to the source image directory
+    max : int
+        Max value for hysteresis thresholding
+    min : int
+        Min value for hysteresis thresholding
+    '''
+    window_name = 'win'
+
+    # Glob images
+    img_paths, ok = images.list(base_path)
+    if not ok:
+        return
+
+    # Display found images
+    click.echo(f'Found <{len(img_paths)}> image(s)')
+    images.print_list(img_paths)
+
+    index = inp.enforce_range_input(
+        f'Enter number between 1 and {len(img_paths)} to select image to use: ', 1, len(img_paths))
+
+    img = cv.imread(img_paths[index-1])
+    cv.imshow(window_name, img)
+
+    wait.wait(10)
--- assign2/main.py
+++ assign2/main.py
@@ -12,21 +12,26 @@ def cli():
     pass
 
 
-@click.command("line")
-@click.option('-p', '--path', default='.data', help='Path to the source directory')
+@click.command('line')
+@click.option('-p', '--path', default='.data', help='Path to the source image directory', type=str)
 def line_cmd(path: str):
     '''Compute the vanishing line based on a selected plane'''
     line.do(path)
 
 
+@click.command('calc')
 def calc_cmd():
     '''Calculate the height of the mug'''
     calc.do()
 
 
-def edges_cmd():
+@click.command('edges')
+@click.option('-p', '--path', default='.data', help='Path to the source image directory', type=str)
+@click.option('-u', '--max', default=200, help='Max value for hysteresis thresholding', type=int)
+@click.option('-l', '--min', default=100, help='Min value for hysteresis thresholding', type=int)
+def edges_cmd(path: str, max: int, min: int):
     '''Automatic edge detection to calculate the vanishing line'''
-    edges.do()
+    edges.do(path, max, min)
 
 
 cli.add_command(edges_cmd)
