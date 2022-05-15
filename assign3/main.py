393c3529331eb06ffab54cf5c1d1051467793604
Techassi <git@techassi.dev>
2022-05-15 03:25:52 +0200

    Re-group commands

--- assign3/main.py
+++ assign3/main.py
@@ -4,16 +4,32 @@ import click
 import cmd.features as features
 import cmd.matrix as matrix
 
-
-@click.group()
-def cli():
-    pass
-
 # Intrinsische Matrix aus EXIF Daten
 # Fundamental Matrix erstellen
 # Rektifizieren
 # Stereo Algos anwenden
 # Depth map
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.group('features')
+def features_group():
+    '''
+    Detect various features in images.
+    '''
+    pass
+
+
+@cli.group('map')
+def map_group():
+    '''
+    Compute depth maps.
+    '''
+    pass
 
 
 @cli.command('matrix')
@@ -25,16 +41,39 @@ def matrix_cmd(path: str):
     matrix.execute(path)
 
 
-@cli.command('features')
+@features_group.command('lines')
 @click.option('-p', '--path', default='.data', help='Path to source images', type=str, show_default=True)
 @click.option('--preview', default=False, help='Show preview windows', type=bool, show_default=True, is_flag=True)
-def features_cmd(path: str, preview: bool):
-    ''''''
-    features.execute(path, preview)
+def epilines_cmd(path: str, preview: bool):
+    '''
+    Extract epipolar lines from two or more images.
+    '''
+    features.epilines(path, preview)
 
 
-@cli.command('depth')
-def depth_cmd():
+@features_group.command('points')
+@click.option('-p', '--path', default='.data', help='Path to source images', type=str, show_default=True)
+@click.option('--preview', default=False, help='Show preview windows', type=bool, show_default=True, is_flag=True)
+def points_cmd(path: str, preview: bool):
+    '''
+    Extract matching feature points.
+    '''
+    features.points(path, preview)
+
+
+@map_group.command('single')
+def single_map_cmd():
+    '''
+    Compute a single depth map from two images.
+    '''
+    pass
+
+
+@map_group.command('multi')
+def multi_map_cmd():
+    '''
+    Combine several depth maps from two images.
+    '''
     pass
 
 
